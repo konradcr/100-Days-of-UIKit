40f5f142c720d156e1897ca12231b28ac1b3ee25
konradcr <63312007+konradcr@users.noreply.github.com>
2022-04-12 11:10:28 +0200

    Project 16 finished

---
+++ Project16_Capital_Cities/Project16_Capital_Cities/Capital.swift
@@ -0,0 +1,21 @@
+//
+//  Capital.swift
+//  Project16_Capital_Cities
+//
+//  Created by Konrad Cureau on 12/04/2022.
+//
+
+import MapKit
+import UIKit
+
+class Capital: NSObject, MKAnnotation {
+    var title: String?
+    var coordinate: CLLocationCoordinate2D
+    var info: String
+    
+    init(title: String, coordinate: CLLocationCoordinate2D, info: String) {
+        self.title = title
+        self.coordinate = coordinate
+        self.info = info
+    }
+}
---
+++ Project16_Capital_Cities/Project16_Capital_Cities/ViewController.swift
@@ -0,0 +1,76 @@
+//
+//  ViewController.swift
+//  Project16_Capital_Cities
+//
+//  Created by Konrad Cureau on 12/04/2022.
+//
+
+import MapKit
+import UIKit
+
+class ViewController: UIViewController, MKMapViewDelegate {
+    @IBOutlet var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let london = Capital(title: "London", coordinate: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275), info: "Home to the 2012 Summer Olympics.")
+        let oslo = Capital(title: "Oslo", coordinate: CLLocationCoordinate2D(latitude: 59.95, longitude: 10.75), info: "Founded over a thousand years ago.")
+        let paris = Capital(title: "Paris", coordinate: CLLocationCoordinate2D(latitude: 48.8567, longitude: 2.3508), info: "Often called the City of Light.")
+        let rome = Capital(title: "Rome", coordinate: CLLocationCoordinate2D(latitude: 41.9, longitude: 12.5), info: "Has a whole country inside it.")
+        let washington = Capital(title: "Washington DC", coordinate: CLLocationCoordinate2D(latitude: 38.895111, longitude: -77.036667), info: "Named after George himself.")
+        
+        mapView.addAnnotations([london, oslo, paris, rome, washington])
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "map.fill"), style: .plain, target: self, action: #selector(changeMap))
+
+    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        guard annotation is Capital else { return nil }
+        
+        let identifier = "Capital"
+        
+        var annotationView = MKMarkerAnnotationView()
+        
+        if let dequeudView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier) as? MKMarkerAnnotationView {
+            annotationView = dequeudView
+            annotationView.annotation = annotation
+        } else {
+            annotationView = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+            
+            annotationView.canShowCallout = true
+            annotationView.markerTintColor = .green
+            
+            let btn = UIButton(type: .detailDisclosure)
+            annotationView.rightCalloutAccessoryView = btn
+        }
+        
+
+        
+        return annotationView
+    }
+    
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        guard let capital = view.annotation as? Capital else { return }
+        
+        let placeName = capital.title
+        let placeInfo = capital.info
+        
+        let ac = UIAlertController(title: placeName, message: placeInfo, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+    }
+    
+    @objc func changeMap() {
+        let ac = UIAlertController(title: "Map style", message: "Which map style do you want?", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Normal", style: .default, handler: { [weak self] _ in
+            self?.mapView.mapType = .standard
+        }))
+        ac.addAction(UIAlertAction(title: "Satellite", style: .default, handler: { [weak self] _ in
+            self?.mapView.mapType = .satellite
+        }))
+        present(ac, animated: true)
+    }
+}
+
